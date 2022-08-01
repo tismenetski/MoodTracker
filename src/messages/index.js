d8d9ab8d377fbe74d370e2161835ef6654b1c790
tismenetski <tismenetski@gmail.com>
2022-08-01 12:24:12 +0300

    created messages file to hold all user messages in the backend

---
+++ src/messages/index.js
@@ -0,0 +1,8 @@
+module.exports  = {
+    invalid_name_empty : "Name cannot be empty",
+    invalid_name_length: "Name must be at least 3 characters long",
+    invalid_email : "The Email provided is invalid, please provide valid email",
+    invalid_password_empty : "Password cannot be empty",
+    invalid_password_length : "Password should be at least 8 characters long",
+    invalid_password_structure : "Password should contain small letter, Capital letter and a number"
+}
