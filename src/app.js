0bee7f42e0919981a18aec2401bf91d4b993bfb4
tismenetski <tismenetski@gmail.com>
2022-08-11 11:27:51 +0300

    Diary and Page Router and messages updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const UserRouter = require('../src/user/UserRouter');
+const DiaryRouter = require('./diary/DiaryRouter');
+const PageRouter= require('./page/PageRouter');
 const errorHandler = require('./error/ErrorHandler');
 
 const app = express();
@@ -7,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(UserRouter);
-
+app.use(DiaryRouter);
+app.use(PageRouter);
 app.use(errorHandler);
 module.exports = app;
